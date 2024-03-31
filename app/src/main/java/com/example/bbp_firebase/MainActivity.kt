c6b77668ecabb2cfadc2b672153439f818c7b983
bagus22520014 <baguspw17@gmail.com>
2024-03-31 10:42:05 +0700

    Tugas Pertemuan 4

--- app/src/main/java/com/example/bbp_firebase/MainActivity.kt
+++ app/src/main/java/com/example/bbp_firebase/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         }
                     })
             R.id.showdata -> {
+                startActivity(Intent(this@MainActivity, MyListData::class.java))
             }
         }
     }
